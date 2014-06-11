97d3a3684bb2e254e1698906ed45bb91d275a5d7
Denis Sokolov <denis@sokolov.cc>
2014-06-11 19:53:35 +0900

    Linting: use strict mode

--- frozen.js
+++ frozen.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var through = require('through2');
 
 module.exports = function() {
--- test/simple.js
+++ test/simple.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var assert = require('better-assert');
 var through = require('through2');
 
