e9db40e9db9f905b9b2fd11dceb6632c68dac3d2
Denis Sokolov <denis@sokolov.cc>
2014-06-12 16:25:24 +0900

    Test: create a convenience lib

--- test/simple.js
+++ test/simple.js
@@ -1,33 +1,32 @@
 'use strict';
 
-var assert = require('better-assert');
 var express = require('express');
-var through = require('through2');
 
 var frozen = require('..');
+var util = require('./util.js');
 
 /* global describe, it */
 
+var lib = {
+	hello: {
+		register: function(app) {
+			app.get(this.path, function(req, res) {
+				res.send('Hello world!');
+			});
+		},
+		path: '/hello',
+		expect: {path: 'hello.html', contents: 'Hello world!'}
+	}
+};
+
 describe('simplest use cases', function(){
 	it('should return a single static file', function(done){
-		var result = [];
-
-		var pipe = through.obj(function(data, enc, next){
-			result.push(data);
-			next();
-		}, function(){
-			assert(result.length === 1);
-			assert(result[0].path.substr(result[0].base.length) === '/hello.html');
-			assert(result[0].contents.toString() === 'Hello world!');
-			done();
-		});
-
 		var app = express();
-		app.get('/hello', function(req, res){
-			res.send('Hello world!');
-		});
-		frozen(app, {
-			routes: ['/hello']
-		}).pipe(pipe);
+		lib.hello.register(app);
+		util.checkPipeContents(
+			frozen(app, {routes: [lib.hello.path]}),
+			[lib.hello.expect],
+			done
+		);
 	});
 });
---
+++ test/util.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var through = require('through2');
+
+var api = {};
+
+api.checkPipeContents = function(pipe, contents, done){
+	var result = [];
+
+	pipe.pipe(through.obj(function(data, enc, next){
+		result.push(data);
+		next();
+	}, function(){
+		if (contents.length !== result.length) {
+			throw new Error('Expected '+contents.length+' items, got '+result.length);
+		}
+		contents.forEach(function(target){
+			var found = result.some(function(attempt){
+				if (target.path && attempt.path.substr(attempt.base.length) !== '/' + target.path) {
+					return false;
+				}
+				if (target.contents && attempt.contents.toString() !== target.contents) {
+					return false;
+				}
+				return true;
+			});
+			if (!found) {
+				console.warn('Did not find', target, 'among', contents);
+				throw new Error('Did not find an expected item in the pipe');
+			}
+		});
+		done();
+	}));
+};
+
+
+module.exports = api;
