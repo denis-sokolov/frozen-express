48283729ffdfff0a6aaec5a6d17f791626262746
Denis Sokolov <denis@sokolov.cc>
2014-06-13 13:35:04 +0900

    Test: refactor

--- test/simple.js
+++ test/simple.js
@@ -5,67 +5,27 @@ var express = require('express');
 var frozen = require('..');
 var util = require('./util.js');
 
-/* global describe, it */
+var test = util.test(frozen, express);
 
-var lib = {
-	hello: {
-		register: function(app) {
-			app.get(this.path, function(req, res) {
-				res.send('Hello world!');
-			});
-		},
-		path: '/hello',
-		expect: {path: 'hello.html', contents: 'Hello world!'}
-	},
-	index: {
-		register: function(app) {
-			app.get(this.path, function(req, res){
-				res.send('Welcome!');
-			});
-		},
-		path: '/',
-		expect: {path: 'index.html', contents: 'Welcome!'}
-	},
-	goodbye: {
-		register: function(app) {
-			app.get(this.path, function(req, res){
-				res.send('Goodbye!');
-			});
-		},
-		path: '/goodbye',
-		expect: {path: 'goodbye.html', contents: 'Goodbye!'}
-	}
-};
+/* global describe, it */
 
 describe('simplest use cases', function(){
 	it('should return a single static file', function(done){
-		var app = express();
-		lib.hello.register(app);
-		util.checkPipeContents(
-			frozen(app, {routes: [lib.hello.path]}),
-			[lib.hello.expect],
-			done
-		);
+		test(done)
+			.route('/hello', 'hello.html', 'Hello world!')
+			.run();
 	});
 
 	it('should return two static files', function(done){
-		var app = express();
-		lib.hello.register(app);
-		lib.goodbye.register(app);
-		util.checkPipeContents(
-			frozen(app, {routes: [lib.goodbye.path, lib.hello.path]}),
-			[lib.hello.expect, lib.goodbye.expect],
-			done
-		);
+		test(done)
+			.route('/hello', 'hello.html', 'Hello world!')
+			.route('/goodbye', 'goodbye.html', 'Goodbye!')
+			.run();
 	});
 
 	it('should handle root index path', function(done){
-		var app = express();
-		lib.index.register(app);
-		util.checkPipeContents(
-			frozen(app, {routes: [lib.index.path]}),
-			[lib.index.expect],
-			done
-		);
+		test(done)
+			.route('/', 'index.html', 'Hello world!')
+			.run();
 	});
 });
--- test/util.js
+++ test/util.js
@@ -1,37 +1,86 @@
 'use strict';
 
+var Promise = require('promise');
 var through = require('through2');
 
 var api = {};
 
-api.checkPipeContents = function(pipe, contents, done){
-	var result = [];
+var lib = {};
 
-	pipe.pipe(through.obj(function(data, enc, next){
-		result.push(data);
-		next();
-	}, function(){
-		if (contents.length !== result.length) {
-			throw new Error('Expected '+contents.length+' items, got '+result.length);
-		}
-		contents.forEach(function(target){
-			var found = result.some(function(attempt){
-				if (target.path && attempt.relative !== target.path) {
-					return false;
-				}
-				if (target.contents && attempt.contents.toString() !== target.contents) {
-					return false;
-				}
-				return true;
-			});
-			if (!found) {
-				console.warn('Did not find', target, 'among', result);
-				throw new Error('Did not find an expected item in the pipe');
-			}
+/**
+ * Create an express app using route data
+ * @param express library
+ * @param [{url}] routes
+ * @return express app
+ */
+lib.makeapp = function(express, routes) {
+	var app = express();
+	routes.forEach(function(route){
+		app.get(route.url, function(req, res){
+			res.send(route.contents);
 		});
-		done();
-	}));
+	});
+	return app;
 };
 
+/**
+ * Get the contents of a pipe
+ * @param pipe
+ * @return Promise([contents])
+ */
+lib.pipeContents = function(pipe){
+	return new Promise(function(resolve){
+		var result = [];
+		pipe.pipe(through.obj(function(data, enc, next){
+			result.push(data);
+			next();
+		}, function(){
+			resolve(result);
+		}));
+	});
+};
+
+api.test = function(frozen, express){
+	return function(done){
+		var routes = [];
+		var test = {};
+
+		test.route = function(url, path, contents){
+			routes.push({
+				url: url, path: path, contents: contents
+			});
+			return test;
+		};
+
+		test.run = function(){
+			var app = lib.makeapp(express, routes);
+			lib.pipeContents(frozen(app, {
+				routes: routes.map(function(route){
+					return route.url;
+				})
+			})).then(function(results){
+				var missing = routes.filter(function(route){
+					return !results.some(function(attempt){
+						if (attempt.relative !== route.path) {
+							return false;
+						}
+						if (attempt.contents.toString() !== route.contents) {
+							return false;
+						}
+						return true;
+					});
+				});
+				if (missing.length) {
+					console.warn('Did not find', missing, 'among', results);
+					done(new Error('Did not find an expected item in the pipe'));
+				} else {
+					done();
+				}
+			});
+		};
+
+		return test;
+	};
+};
 
 module.exports = api;
