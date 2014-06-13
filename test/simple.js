c1571a2d293c18c8ddfad6fd022762c128778360
Denis Sokolov <denis@sokolov.cc>
2014-06-13 13:35:49 +0900

    Test: remove describe grouping

--- test/simple.js
+++ test/simple.js
@@ -7,25 +7,23 @@ var util = require('./util.js');
 
 var test = util.test(frozen, express);
 
-/* global describe, it */
+/* global it */
 
-describe('simplest use cases', function(){
-	it('should return a single static file', function(done){
-		test(done)
-			.route('/hello', 'hello.html', 'Hello world!')
-			.run();
-	});
+it('should return a single static file', function(done){
+	test(done)
+		.route('/hello', 'hello.html', 'Hello world!')
+		.run();
+});
 
-	it('should return two static files', function(done){
-		test(done)
-			.route('/hello', 'hello.html', 'Hello world!')
-			.route('/goodbye', 'goodbye.html', 'Goodbye!')
-			.run();
-	});
+it('should return two static files', function(done){
+	test(done)
+		.route('/hello', 'hello.html', 'Hello world!')
+		.route('/goodbye', 'goodbye.html', 'Goodbye!')
+		.run();
+});
 
-	it('should handle root index path', function(done){
-		test(done)
-			.route('/', 'index.html', 'Hello world!')
-			.run();
-	});
+it('should handle root index path', function(done){
+	test(done)
+		.route('/', 'index.html', 'Hello world!')
+		.run();
 });
